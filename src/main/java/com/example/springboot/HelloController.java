0a211b50f28a32b2f508331b3c6ff5613a219287
rsattipalli <89206723+rsattipalli@users.noreply.github.com>
2021-09-22 12:31:37 -0500

    added environment variable

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class HelloController {
 
+	@Autowired
+	private Environment env;
+	
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Docker";
+		return "Greetings from Spring Boot! Docker" ;
 	}
-
+	@GetMapping("/env")
+	public String env() {
+		return "Environemnt from property files " + env.getProperty("environment") ;
+	}
 }
