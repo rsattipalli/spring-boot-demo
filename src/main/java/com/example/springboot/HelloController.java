3b254eb12d0d9ce23b90609f0b5b7f10a1372da4
rsattipalli <89206723+rsattipalli@users.noreply.github.com>
2021-10-27 12:28:29 -0500

    value addition

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +14,18 @@ public class HelloController {
 	@Autowired
 	private Environment env;
 	
+	@Value("${spring.application.name}")
+	private String application;
+	
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Docker" ;
+		return "Greetings from Spring Boot! Docker  " + application ;
+		
 	}
 	@GetMapping("/env")
 	public String env() {
 		return "Environemnt from property files " + env.getProperty("environment") ;
+		//return "Environemnt from property files " + testenv ;
+		
 	}
 }
