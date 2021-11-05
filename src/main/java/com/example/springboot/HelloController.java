78507def7a66369dd55832247af066df4af457d9
rsattipalli <89206723+rsattipalli@users.noreply.github.com>
2021-11-05 14:12:58 -0500

    ssl backend test

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,10 +1,15 @@
 package com.example.springboot;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 
 
@@ -13,6 +18,9 @@ public class HelloController {
 
 	@Autowired
 	private Environment env;
+	
+	@Autowired
+	RestTemplate restTemplate;
 	
 	@Value("${spring.application.name}")
 	private String application;
@@ -27,6 +35,25 @@ public class HelloController {
 		return "Environemnt from property files " + env.getProperty("environment") ;
 		//return "Environemnt from property files " + testenv ;
 		
+	}
+	@RequestMapping(value = "/data", method = RequestMethod.GET)
+	public String getData() {
+		System.out.println("Returning data from nt-gateway own data method");
+		return "Hello from NT-GATEWAY-data method";
+	}
+	
+	@RequestMapping(value = "/ms-data", method = RequestMethod.GET)
+	public String getMsData() {
+		System.out.println("Got inside NT-GATEWAY-ms-data method");
+		try {
+			String msEndpoint = env.getProperty("endpoint.ms-service");
+			System.out.println("MS Endpoint name : [" + msEndpoint + "]");
+			
+			return restTemplate.getForObject(new URI(msEndpoint), String.class);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return "Exception occurred.. so, returning default data";
 	}
 	
 }
